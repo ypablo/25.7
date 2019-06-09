0078e47cecceea90565715a363508a4d5fbd9d09
ypablo <yfedora@gmail.com>
2019-06-09 15:46:09 +0100

    25.7 API

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "GOOGLE_CLIENT_ID": "69761774572-jfe3htue0t4frj7dasha5ri7ti6elr4s.apps.googleusercontent.com",
+    "GOOGLE_CLIENT_SECRET": "y4lZoanOb5eHNDB7HuvgrZPW",
+    "CALLBACK_URL": "http://localhost:3000/auth/google/callback"
+};

af1adbd17af5f92f6f1c85ea0e9e0393c1931d9e
ypablo <37043186+ypablo@users.noreply.github.com>
2019-06-09 15:56:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "GOOGLE_CLIENT_ID": "69761774572-jfe3htue0t4frj7dasha5ri7ti6elr4s.apps.googleusercontent.com",
-    "GOOGLE_CLIENT_SECRET": "y4lZoanOb5eHNDB7HuvgrZPW",
+    "GOOGLE_CLIENT_ID": ",
+    "GOOGLE_CLIENT_SECRET": "",
     "CALLBACK_URL": "http://localhost:3000/auth/google/callback"
 };
